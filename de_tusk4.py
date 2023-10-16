585f56dd4ef719ff25a08385c3280d4111e8358a
Danila <danila200093@gmail.com>
2023-10-16 12:54:37 +0500

    commit

--- de_tusk4.py
+++ de_tusk4.py
@@ -12,12 +12,26 @@ with open(file_name, 'r', encoding="UTF-8")as csv_file:
 
 for i in range(len(lines)):
     avg_salary += int(lines[i][-1][:-1])
+    lines[i][1] = " ".join([lines[i][1], lines[i][2]])
+    lines[i].pop(2)
 avg_salary /= len(lines)
 
-for i in range(len(lines)-i):
+# Фильтр по доходу больше среднего
+for i in range(len(lines)-1, -1, -1):    
     if int(lines[i][-1][:-1]) < avg_salary:
-        lines[i]
+        lines.pop(i)
+
+# Фильтр по возрасту больше 25, вариант №10
+for i in range(len(lines)-1, -1, -1):    
+    if int(lines[i][-2]) <= 25:
+        lines.pop(i)
+
+lines = sorted(lines, key=lambda row: int(row[0]))
+
+tmp = lines.copy()
 
 
-for line in lines:
-    print(line)
+
+with open(f"out_{file_name}", "w", encoding="UTF-8", newline='') as csv_f:
+    wrt = csv.writer(csv_f)
+    wrt.writerows(lines)
