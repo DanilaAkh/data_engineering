deb4728f969955ffbca5639e2674dad89f56f5fd
Danila <danila200093@gmail.com>
2023-12-28 12:25:32 +0500

    add pr_w6

--- practical_work6/plot_file1.py
+++ practical_work6/plot_file1.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import matplotlib
 import matplotlib.pyplot as plt
 import json
 import numpy as np
@@ -20,57 +21,71 @@ def read_dtypes(file_name):
 
 
 # Построение графика количества игр по дням недели
-def first_plot(dataset):
+def first_plot(dataset : pd.DataFrame):
     plt.figure(figsize=(10, 5))
     cats = ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun']
     df = dataset.copy()
     df["day_of_week"] = pd.Categorical(dataset['day_of_week'], categories=cats, ordered=True)    
-    plot = dataset.groupby(df["day_of_week"], observed=True)["number_of_game"].mean().plot(title="Среднее количество игр по дням недели")
+    plot = dataset.groupby(df["day_of_week"], observed=True)["number_of_game"].sum().plot(kind="bar", title="Среднее количество игр в день")
     plot.get_figure().savefig(".\\file1_fig\\1.png")
     plt.close()
 
 
-
-
-def main():
-    need_dtypes = read_dtypes("dtypes.json")
-    dataset = pd.read_csv("df.csv",
-                          #usecols=lambda x: x in need_dtypes.keys(),
-                          dtype=need_dtypes)    
-    #dataset.info(memory_usage="deep")
-
-    #print(dataset)
-    
-    
-    
-    
-    # Построение графика Hits
+def second_plot(dataset : pd.DataFrame):
+    #Построение графика Hits
     plt.figure(figsize=(10, 5))
     for col in dataset[[col for col in dataset.columns if "hits" in col]]:
         plot = dataset[col].plot(bins=100, kind='hist', title="Hits", alpha=0.7, legend=True)
     plot.get_figure().savefig(".\\file1_fig\\2.png")
     plt.close()
 
+
+def third_plot(dataset : pd.DataFrame):
     # Количество игр в виде круговой диаграммы
-    plt.figure(figsize=(10, 5))    
-    plot = dataset["number_of_game"].value_counts().plot(kind='pie', title='Количество игр')
+    plt.figure(figsize=(10, 10))
+    explode = (0.1, 0.15, 0.1, 0.15)
+    plot = dataset["number_of_game"].value_counts().plot(kind='pie',  autopct="%1.1f%%", 
+                                                         explode=explode, fontsize=18)
+    plt.ylabel("")
+    plt.title('Количество игр', fontsize=20)
     plot.get_figure().savefig(".\\file1_fig\\3.png")
+    
     plt.close()
 
-    # Корреляция
-    df = dataset.select_dtypes(include=[int, float])
+
+def fourth_plot(dataset : pd.DataFrame):
+    # Корреляция    
+    df = dataset.select_dtypes(include=[np.uint, float])
     plt.figure(figsize=(16,16))
+    matplotlib.rc('font', size=18)
     sns.heatmap(df.corr(), annot=True, cmap="YlGnBu", cbar=False)
+    plt.title("Корреляция", fontsize=20)
     plt.savefig(".\\file1_fig\\4.png")
     plt.close()
+    return df
 
-    # pairplot
+
+def fifth_plot(df : pd.DataFrame):
+    #pairplot
     sns.pairplot(df)
     plt.savefig(".\\file1_fig\\5.png")
 
 
-    plt.show()
+def main():
+    need_dtypes = read_dtypes(".\\out1\\dtypes1.json")
+    dataset = pd.read_csv(".\\out1\\df1.csv",
+                          #usecols=lambda x: x in need_dtypes.keys(),
+                          dtype=need_dtypes)    
+    #dataset.info(memory_usage="deep")
 
+    #print(dataset)
+    
+    first_plot(dataset)
+    second_plot(dataset)
+    third_plot(dataset)
+    df = fourth_plot(dataset)
+    fifth_plot(df)
+    
 
 if __name__ == "__main__":
     main()
